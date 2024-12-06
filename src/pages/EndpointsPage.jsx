02db081440b15c5c6055d6ce8e4ac99717176e18
AllanChandler <cph-ac326@cphbusiness.dk>
2024-12-06 14:31:52 +0100

    fix

--- src/pages/EndpointsPage.jsx
+++ src/pages/EndpointsPage.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const EndpointsTable = () => {
   const endpoints = [
     {
--- src/pages/VisionPage.jsx
+++ src/pages/VisionPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const VisionWrapper = styled.div`
@@ -11,9 +10,10 @@ const VisionPage = () => {
     <VisionWrapper>
       <h1>Vision of JourneyHub API</h1>
       <p>
-      JourneyHub API giver brugerne adgang til et rejsesystem, hvor de kan finde destinationer, booke rejser og administrere deres planer.
-       API'et gør det også muligt at skrive og læse anmeldelser af forskellige destinationer, hvilket skaber en platform for feedback og anbefalinger. 
-       Uanset om du vil finde din næste rejse eller dele dine oplevelser, gør JourneyHub API det nemt at interagere med rejseverdenen.      </p>
+        JourneyHub API giver brugerne adgang til et rejsesystem, hvor de kan finde destinationer, booke rejser og administrere deres planer.
+        API&apos;et gør det også muligt at skrive og læse anmeldelser af forskellige destinationer, hvilket skaber en platform for feedback og anbefalinger.
+        Uanset om du vil finde din næste rejse eller dele dine oplevelser, gør JourneyHub API det nemt at interagere med rejseverdenen.
+      </p>
     </VisionWrapper>
   );
 };
